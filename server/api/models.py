0c337762e27b7f78df62a7d25c8bf8e54ce8b7d4
saqibcare <saqibcare@gmail.com>
2018-12-26 14:16:57 +0100

    Adding client design

--- server/api/models.py
+++ server/api/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Host(models.Model):
+    fName = models.CharField(max_length=20)
+    lName = models.CharField(max_length=20)
+    dob = models.DateField(auto_now=False, auto_now_add=False)
+    email = models.EmailField(max_length=254)
+
+
+class Links(models.Model):
+    title = models.CharField(max_length=20)
+    url = models.URLField()
+
+
+
+
